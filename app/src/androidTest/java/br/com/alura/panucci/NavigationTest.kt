e8c9f1eacb2e5ef6ce98512e484f90055d2b18d4
“William8Cabral” <suporte@vxm.com.br>
2025-10-06 11:37:52 -0300

    test navigation, log node

---
+++ app/src/androidTest/java/br/com/alura/panucci/NavigationTest.kt
@@ -0,0 +1,59 @@
+package br.com.alura.panucci
+
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.performClick
+import androidx.compose.ui.test.printToLog
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.testing.TestNavHostController
+import br.com.alura.panucci.navigation.PanucciNavHost
+import br.com.alura.panucci.navigation.drinksRoute
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class NavigationTest {
+
+    @get:Rule
+    val composeTestRule = createComposeRule()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupAppNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current)
+            navController.navigatorProvider.addNavigator(ComposeNavigator())
+            PanucciNavHost(navController = navController)
+        }
+    }
+
+    @Test
+    fun appNavHost_verifyStartDestination() {
+        //NOTE Log dos nós
+        composeTestRule.onRoot().printToLog("panucci app")
+
+        composeTestRule
+            .onNodeWithText("Destaques do dia")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun appNavHost_verifyIfMenuScreenIsDisplayed() {
+        //NOTE Log dos nós
+        composeTestRule.onRoot().printToLog("panucci app")
+        composeTestRule.onNodeWithText("Menu")
+            .performClick()
+
+        composeTestRule.onNodeWithText("Menu")
+            .assertIsDisplayed()
+
+        val route = navController.currentBackStackEntry?.destination?.route
+        assertEquals(route, drinksRoute)
+    }
+
+}
